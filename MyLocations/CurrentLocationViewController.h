8dba8e527704fe1b3d76deec2e92faf3a016b471
Maijid Moujaled <maijid@gmail.com>
2011-12-19 11:19:01 -0600

    Changed ViewController name

---
+++ MyLocations/CurrentLocationViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  MyLocations
+//
+//  Created by Maijid  Moujaled (aka. Dr.Jid) on 12/19/11.
+//  Copyright (c) 2011 Grinnell College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CurrentLocationViewController : UIViewController
+
+@end
